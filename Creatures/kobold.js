88c20f9e5dcd7b87f40e258f224ba81fe2ee5d0c
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-06-04 12:14:08 -0500

    feat: :white_check_mark: adding stuff

---
+++ Creatures/kobold.js
@@ -0,0 +1,5 @@
+import { monster } from "../Creatures/monster.js";
+
+export class kobold extends monster{
+    
+}
---
+++ Creatures/orco.js
@@ -0,0 +1,5 @@
+import { monster } from "../Creatures/monster.js";
+
+export class orco extends monster{
+    
+}
